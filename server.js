d645c9735c54a12432b69a26cff9b8a1d9f2ddc9
neword <newordn@gmail.com>
2019-04-23 07:53:29 +0200

    Added file description endpoint

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var multer = require('multer');
+var upload = multer({dest : 'uploads/'});
 // require and use "multer"...
 
 var app = express();
@@ -16,6 +17,11 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse',upload.single("upfile"),function(req,res){
+const file =req.file;
+  res.json({"name":file.originalname,"type":file.filename,"size":file.size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
